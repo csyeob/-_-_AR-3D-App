44f03143bbcf0afa0d89f3ca7f0341e59543f39e
csyeob <csyeob@gmail.com>
2022-07-28 19:24:10 +0900

    crawling 구현(네이버 쇼핑 의자 인기검색)

--- Assets/Scripts/crawler.cs
+++ Assets/Scripts/crawler.cs
@@ -2,15 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using HtmlAgilityPack;
-
+using UnityEngine.UI;
+using System.Linq;
+using System.Net;
+using System.IO;
 public class crawler : MonoBehaviour
 {
-    string html = "https://search.shopping.naver.com/search/all?query=게이밍의자";
-    HtmlWeb web = new HtmlWeb();
-
-    public void parser()
+   
+    public Text tx;
+    void Start()
     {
-        HtmlDocument htmlDoc = web.Load(html);
-        
+
+        string url = "https://search.shopping.naver.com/search/all?query=의자";
+        HtmlWeb web = new HtmlWeb();
+        HtmlDocument doc = web.Load(url);
+        int num = 0;
+
+        foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//div[@class='basicList_title__3P9Q7']"))
+        {
+            tx.text += node.InnerText.ToString() + "\n";
+            num++;
+            if(num == 10)
+            {
+                return;
+            }
+        }
+        var rawimageObject = new GameObject("Rawimage");
+        var rawimage = rawimageObject.AddComponent<RawImage>();
+        rawimage.GetComponent<RawImage>().texture = r(cralwer image).GetComponent<RawImage>().texture;
+
     }
+    
 }
