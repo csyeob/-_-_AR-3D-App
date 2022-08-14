deccebd28d049cc462382adc6b9701c370f89def
csyeob <csyeob@gmail.com>
2022-08-14 23:12:24 +0900

    숨김, 보이기 버튼 동작 코딩

--- Assets/Scripts/Panel_Visible.cs
+++ Assets/Scripts/Panel_Visible.cs
@@ -6,14 +6,20 @@ using UnityEngine.UI;
 public class Panel_Visible : MonoBehaviour
 {
     public GameObject panel;
+    public GameObject hide;
+    public GameObject look;
 
     public void visible_panel()
     {
         panel.SetActive(true);
+        hide.SetActive(true);
+        look.SetActive(false);
     }
     public void hidepanel(GameObject p)
     {
         p.SetActive(false);
+        hide.SetActive(false);
+        look.SetActive(true);
     }
 
     public void like()
