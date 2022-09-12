7bd3693aa67f95e0e10d60f6b99016c192237498
csyeob <csyeob@gmail.com>
2022-09-12 20:05:29 +0900

    Google Street View 앱 실행 및 다운로드 창열기

---
+++ Assets/AppLauch.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppLauch : MonoBehaviour
+{
+
+    public void OpenUrl()
+    {
+        Application.OpenURL("https://apps.apple.com/us/app/google-street-view/id904418768");
+    }
+    
+}
