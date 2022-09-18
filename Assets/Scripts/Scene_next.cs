3e5fa5526519f0f5c9f8d4ef89b6cb01c1143580
csyeob <csyeob@gmail.com>
2022-09-18 23:21:50 +0900

    Scene_next 스크립트 수정

--- Assets/Scripts/Scene_next.cs
+++ Assets/Scripts/Scene_next.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR;
 
 public class Scene_next : MonoBehaviour
 {
@@ -9,5 +10,10 @@ public class Scene_next : MonoBehaviour
    public void scene_next(string scene)
     {
         SceneManager.LoadScene(scene);
+    }
+    public void VR_next(string scene)
+    {
+        SceneManager.LoadScene(scene);
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
     }
 }
